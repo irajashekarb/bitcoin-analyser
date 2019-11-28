259cdef7e7f40ff33d963aeb455a42af90934652
Raja Shekar <irajashekarb@gmail.com>
2019-11-28 21:27:35 +0530

    ClassNotFound Exception

--- build.sbt
+++ build.sbt
@@ -2,8 +2,8 @@ name := "bitcoin-analyser"
 
 version := "0.1"
 
-scalaVersion := "2.11.11"
-val sparkVersion = "2.3.1"
+scalaVersion := "2.12.8"
+val sparkVersion = "2.4.4"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion % Provided,
@@ -19,9 +19,11 @@ libraryDependencies ++= Seq(
   "org.typelevel" %% "cats-core" % "1.1.0",
   "org.typelevel" %% "cats-effect" % "1.0.0-RC2",
   "org.apache.spark" %% "spark-streaming" % sparkVersion % Provided,
-  "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion % Provided exclude ("net.jpountz.lz4", "lz4"),
+  "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion,
   "com.pusher" % "pusher-java-client" % "1.8.0",
-  "org.apache.hadoop" % "hadoop-common" % "2.3.0")
+  "org.apache.hadoop" % "hadoop-common" % "2.3.0",
+  "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.5",
+  "com.fasterxml.jackson.core" % "jackson-core" % "2.9.5")
 
 scalacOptions += "-Ypartial-unification"
 
--- src/main/scala/coinyser/StreamingProducer.scala
+++ src/main/scala/coinyser/StreamingProducer.scala
@@ -11,39 +11,42 @@ import com.pusher.client.Client
 import com.pusher.client.channel.SubscriptionEventListener
 import com.typesafe.scalalogging.StrictLogging
 
-object StreamingProducer extends StrictLogging{
-  val mapper: ObjectMapper = {
+object StreamingProducer extends StrictLogging {
 
-    val m = new ObjectMapper()
-    m.registerModule(DefaultScalaModule)
-    val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-    sdf.setTimeZone(TimeZone.getTimeZone("UTC"))
-    m.setDateFormat(sdf)
-  }
-
-  // Method for deserializing websocket transaction
-  def deserializeWebsocketTransaction(s: String): WebsocketTransaction = {
-    mapper.readValue(s, classOf[WebsocketTransaction])
-  }
-
-  def subscribe(pusher: Client) (onTradeReceived: String => Unit): IO[Unit] = {
+  def subscribe(pusher: Client)(onTradeReceived: String => Unit): IO[Unit] = {
     for {
       _ <- IO(pusher.connect())
       channel <- IO(pusher.subscribe("live_trades"))
-      _ <- IO(channel.bind("trade", new SubscriptionEventListener {
-        override def onEvent(channelName: String, event: String, data: String): Unit = {
-          logger.info(s"Received event $event with data: $data from $channelName")
+
+      _ <- IO(channel.bind("trade", new SubscriptionEventListener() {
+        override def onEvent(channel: String, event: String, data:
+        String): Unit = {
+          logger.info(s"Received event: $event with data: $data")
+          onTradeReceived(data)
         }
       }))
     } yield ()
   }
 
-  def convertWsTransaction(wsTx: WebsocketTransaction): Transaction = {
-    Transaction(
-      timestamp = new Timestamp(wsTx.timestamp.toLong * 1000), tid = wsTx.id, price = wsTx.price, sell = wsTx.`type` == 1, amount = wsTx.amount)
+  val mapper: ObjectMapper = {
+//    println("I'm running!")
+    val m = new ObjectMapper()
+    new ObjectMapper().registerModule(DefaultScalaModule)
+    val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    sdf.setTimeZone(TimeZone.getTimeZone("UTC"))
+    m.setDateFormat(sdf)
   }
 
-  def serializeTransaction(transaction: Transaction): String = {
-    mapper.writeValueAsString(transaction)
+  def deserializeWebsocketTransaction(s: String): WebsocketTransaction = {
+    mapper.readValue(s, classOf[WebsocketTransaction])
   }
+
+  def convertWsTransaction(wsTx: WebsocketTransaction): Transaction =
+    Transaction(
+      timestamp = new Timestamp(wsTx.timestamp.toLong * 1000), tid =
+        wsTx.id, price = wsTx.price, sell = wsTx.`type` == 1, amount =
+        wsTx.amount)
+
+  def serializeTransaction(tx: Transaction): String =
+    mapper.writeValueAsString(tx)
 }
